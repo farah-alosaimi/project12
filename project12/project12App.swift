c3d305e9ec6353632a3c128ad55a9529905cc9ca
farah-alosaimi <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>
2023-10-08 12:13:13 +0300

    Initial Commit

---
+++ project12/project12App.swift
@@ -0,0 +1,17 @@
+//
+//  project12App.swift
+//  project12
+//
+//  Created by Farah Hasan Alosaimi on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct project12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
